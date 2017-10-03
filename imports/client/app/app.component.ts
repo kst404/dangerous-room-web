12330933af2d3c5bfead2d1ac255fdabc753c452
Andrey Kartashov <porter@porter.st>
2017-10-03 10:21:03 -0400

    Styling

--- imports/client/app/app.component.ts
+++ imports/client/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'dr-app',
-  template: `
-    <router-outlet></router-outlet>
-  `
+    selector: 'dr-app',
+    template: `
+        <td-layout>
+            <td-layout-card-over cardTitle="Dangerous Room" cardSubtitle="It's really dangerous" cardWidth="75">
+                <md-card-content>
+                    <router-outlet></router-outlet>
+                </md-card-content>
+            </td-layout-card-over>
+        </td-layout>
+    `
 })
 export class AppComponent {}
